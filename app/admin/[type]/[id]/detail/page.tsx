7f3b7d6b2bc8c903c76d57f073694abfa7f8ec45
nari <yasunarj@gmail.com>
2025-02-15 17:41:04 +0900

    done

--- app/admin/[type]/[id]/detail/page.tsx
+++ app/admin/[type]/[id]/detail/page.tsx
@@ -1,12 +1,13 @@
 import ReservationDetailCard from "@/components/detail/reservationCard";
 import { getReservationByTypeWithId } from "@/lib/getReservationsByType";
-
-const ReservationDetailPage = async ({
-  params,
-}: {
-  params: { type: string; id: string };
-}) => {
-  const { type, id } = await params;
+type ReservationDetailPageProps = {
+  params: {
+    type: string;
+    id: string;
+  };
+};
+const ReservationDetailPage = async ({ params }: ReservationDetailPageProps) => {
+  const { type, id } = params;
   const reservationId = parseInt(id, 10);
   const decodeType = decodeURIComponent(type);
   const reservation = await getReservationByTypeWithId(
@@ -19,7 +20,7 @@ const ReservationDetailPage = async ({
       <div
         className="flex-grow flex justify-center items-center bg-center bg-cover"
         style={{
-          backgroundImage: "url('/images/istockphoto-1499955814-612x612.jpg')", 
+          backgroundImage: "url('/images/istockphoto-1499955814-612x612.jpg')",
         }}
       >
         <div className="w-[80%] h-[95%] bg-white rounded-xl shadow-4xl p-4">
