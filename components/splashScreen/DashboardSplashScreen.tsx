385f75b86967607d4b1b6b9e45e823b509507a1c
nari <yasunarj@gmail.com>
2025-02-23 16:45:11 +0900

    fix 画面先行表示修正

--- components/splashScreen/DashboardSplashScreen.tsx
+++ components/splashScreen/DashboardSplashScreen.tsx
@@ -18,7 +18,7 @@ const DashboardSplashScreen = () => {
           initial={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 1 }}
-          className="fixed top-[5%] h-[90%] sm:top-[4.6%] left-0 w-full sm:h-[90.8%] bg-gradient-to-b from-gray-200 to-gray-100 z-50"
+          className="fixed top-[5.2%] h-[89.8%] sm:top-[4.6%] left-0 w-full sm:h-[90.8%] bg-gradient-to-b from-gray-200 to-gray-100 z-50"
         ></motion.div>
       )}
     </AnimatePresence>
