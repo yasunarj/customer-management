c1a7339fc0dd8c3b3b1a74f0fb8d5988d12f6ed2
nari <yasunarj@gmail.com>
2025-02-23 18:44:01 +0900

    dashboard修正

--- components/layout/AdminDashboardLayout.tsx
+++ components/layout/AdminDashboardLayout.tsx
@@ -1,38 +1,29 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { ReactNode } from "react";
 import DashboardSplashScreen from "@/components/splashScreen/DashboardSplashScreen";
 const AdminDashboardLayout = ({ children }: { children: ReactNode }) => {
   const [isLoading, setIsLoading] = useState(false);
-  const [isSplashVisible, setIsSplashVisible] = useState(false);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsSplashVisible(true);
-    }, 400);
-    return () => clearTimeout(timer);
-  }, []);
   return (
     <>
       <DashboardSplashScreen />
-      {isSplashVisible && (
-        <div className="relative w-full h-screen-vh overflow-hidden">
-          <video
-            className="absolute top-0 left-0 w-full h-full object-cover brightness-75"
-            src="/videos/5580_960x540.mp4"
-            onLoadedData={() => setIsLoading(true)}
-            loop
-            autoPlay
-            muted
-            playsInline
-          />
-          {isLoading && (
-            <div className="relative z-10 h-full flex justify-center items-center">
-              {children}
-            </div>
-          )}
-        </div>
-      )}
+      <div className="relative w-full h-screen-vh overflow-hidden">
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover brightness-75"
+          src="/videos/5580_960x540.mp4"
+          onLoadedData={() => setIsLoading(true)}
+          loop
+          autoPlay
+          muted
+          playsInline
+        />
+        {isLoading && (
+          <div className="relative z-10 h-full flex justify-center items-center">
+            {children}
+          </div>
+        )}
+      </div>
     </>
   );
 };
