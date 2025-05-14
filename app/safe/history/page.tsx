2f575188e2dc6925a17360dd258bc40185e0fd97
nari <yasunarj@gmail.com>
2025-05-14 12:23:18 +0900

    精算履歴のoverscrollの追加

--- app/safe/history/page.tsx
+++ app/safe/history/page.tsx
@@ -15,49 +15,50 @@ const SafeCheckHistoryPage = async () => {
             <SheetMenu menuList={safeMenuList} />
           </div>
         </h1>
-
-        <table className="table-auto w-[95%] mx-auto border-collapse border border-gray-300 mt-4 text-gray-700 overflow-y-scroll">
-          <thead>
-            <tr>
-              <th className="border border-gray-300 px-4 py-2">日付</th>
-              <th className="border border-gray-300 px-4 py-2">合計金額</th>
-              <th className="border border-gray-300 px-4 py-2"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {safeCheckAllData.length === 0 ? (
+        <div className="h-[90%] overflow-y-scroll">
+          <table className="table-auto w-[95%] mx-auto border-collapse border border-gray-300 mt-4 text-gray-700">
+            <thead>
               <tr>
-                <td colSpan={3} className="text-center py-4 text-gray-500">
-                  データが存在しません
-                </td>
+                <th className="border border-gray-300 px-4 py-2">日付</th>
+                <th className="border border-gray-300 px-4 py-2">合計金額</th>
+                <th className="border border-gray-300 px-4 py-2"></th>
               </tr>
-            ) : (
-              safeCheckAllData.map((safeData) => {
-                return (
-                  <tr key={safeData.id} className="text-center">
-                    <td className="border border-gray-300 px-4 py-2">
-                      {new Date(safeData.date).toLocaleDateString("ja-JP")}
-                    </td>
-                    <td
-                      className={`border border-gray-300 px-4 py-2 ${
-                        safeData.total !== 300000
-                          ? "text-red-600"
-                          : "text-blue-600"
-                      }`}
-                    >
-                      {safeData.total}
-                    </td>
-                    <td className="border border-gray-300 px-4 py-2">
-                      <Link href={`/safe/history/${safeData.id}/detail`} className="underline underline-offset-4 hover:text-black">
-                        詳細
-                      </Link>
-                    </td>
-                  </tr>
-                );
-              })
-            )}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {safeCheckAllData.length === 0 ? (
+                <tr>
+                  <td colSpan={3} className="text-center py-4 text-gray-500">
+                    データが存在しません
+                  </td>
+                </tr>
+              ) : (
+                safeCheckAllData.map((safeData) => {
+                  return (
+                    <tr key={safeData.id} className="text-center">
+                      <td className="border border-gray-300 px-4 py-2">
+                        {new Date(safeData.date).toLocaleDateString("ja-JP")}
+                      </td>
+                      <td
+                        className={`border border-gray-300 px-4 py-2 ${
+                          safeData.total !== 300000
+                            ? "text-red-600"
+                            : "text-blue-600"
+                        }`}
+                      >
+                        {safeData.total}
+                      </td>
+                      <td className="border border-gray-300 px-4 py-2">
+                        <Link href={`/safe/history/${safeData.id}/detail`} className="underline underline-offset-4 hover:text-black">
+                          詳細
+                        </Link>
+                      </td>
+                    </tr>
+                  );
+                })
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
