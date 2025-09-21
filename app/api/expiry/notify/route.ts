e2a140fb85f2ac2363fcaf1a4069aec5f0dc0211
nari <yasunarj@gmail.com>
2025-09-21 16:25:18 +0900

    fixed: notify

--- app/api/expiry/notify/route.ts
+++ app/api/expiry/notify/route.ts
@@ -66,7 +66,8 @@ export async function GET() {
       </div>
     `;
 
-    const to = [process.env.TO_EMAIL, process.env.GMAIL_USER].filter(Boolean) as string[];
+    const to = [process.env.GMAIL_USER].filter(Boolean) as string[];
+    // const to = [process.env.TO_EMAIL, process.env.GMAIL_USER].filter(Boolean) as string[];
 
     await sendMail({
       to,
