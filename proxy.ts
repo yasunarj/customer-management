8f76cd4c7394b3f646807bd122c964574a1bd4ae
Takahashi Yasunari <yasunarj@gmail.com>
2026-01-28 19:11:37 +0900

    fixed: middleware.ts -> proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const response = await updateSession(request);
   // return await updateSession(request);
 
