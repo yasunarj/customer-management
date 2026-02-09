a550312ded955bf894407899494a024d27b585c3
Takahashi Yasunari <yasunarj@gmail.com>
2026-02-09 17:18:34 +0900

    fixed: layout.tsx height

--- app/lp/components/Hero.tsx
+++ app/lp/components/Hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
   }, []);
 
   return (
-    <div className={`relative w-full h-screen-vh overflow-hidden transition-opacity duration-1000 ${isVisibleImage ? "opacity-100" : "opacity-0"}`}>
+    <div className={`relative w-full min-h-[100dvh] transition-opacity duration-1000 ${isVisibleImage ? "opacity-100" : "opacity-0"}`}>
       {images.map((src, i) => 
       <Image
       key={i}
