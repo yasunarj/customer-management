fdf6c4b6ef103eb84bdd343ae694a491a5451c37
Takahashi Yasunari <yasunarj@gmail.com>
2026-02-18 18:22:46 +0900

    add: history&detail loading.tsx

---
+++ app/safe/history/[id]/detail/loading.tsx
@@ -0,0 +1,13 @@
+const Loading = () => {
+  return (
+    <div className="h-screen-vh overflow-hidden bg-blue-200 flex justify-center items-center">
+      <div className="bg-white w-[90%] h-[95%] rounded-xl overflow-y-scroll shadow-2xl flex justify-center items-center">
+        <div className="text-gray-700 text-xl font-semibold">
+          読み込み中...
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
--- app/safe/history/loading.tsx
+++ app/safe/history/loading.tsx
@@ -1,8 +1,8 @@
 const Loading = () => {
   return (
-    <div className="h-screen-vh bg-blue flex justify-center items-center">
+    <div className="h-screen-vh bg-blue-200 flex justify-center items-center">
       <div className="max-w-[900px] w-[80%] h-[80%] bg-white rounded-xl p-6 flex items-center justify-center">
-        <div className="text-gray-700 text-xl font-semibold">読み込み中...</div>
+        ç
       </div>
     </div>
   );
